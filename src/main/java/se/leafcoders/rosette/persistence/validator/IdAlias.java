7fd6fe5d6da018ce7f014e19a4c495e5212586ca
Oskar Bilén <oskar@ebilen.se>
2018-09-17 19:10:48 +0200

    IdAlias validation annotation did not apply its own validations on field

--- src/main/java/se/leafcoders/rosette/persistence/validator/IdAlias.java
+++ src/main/java/se/leafcoders/rosette/persistence/validator/IdAlias.java
@@ -6,16 +6,25 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
-import javax.validation.constraints.NotEmpty;
 import se.leafcoders.rosette.exception.ApiString;
 
 @Documented
 @Target({ ANNOTATION_TYPE, FIELD })
 @Retention(RUNTIME)
+@Constraint(validatedBy = { })
 @NotEmpty(message = ApiString.STRING_NOT_EMPTY)
 @Length(max = 32, message = ApiString.STRING_MAX_32_CHARS)
 @Pattern(regexp = "[a-z][a-zA-Z0-9]+", message = ApiString.IDALIAS_INVALID_FORMAT)
 public @interface IdAlias {
+
+    String message() default "";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default {};
 }
